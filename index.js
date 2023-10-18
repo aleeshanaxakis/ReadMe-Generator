1c67dd3db479ea6bc580100dad450121f4b3048d
Aleesha Naxakis <aleesha-@hotmail.com>
2023-10-19 07:56:28 +1100

    added prompts for each part of README

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 const inquirer = require('inquirer');
+
+inquirer
+.createPromptModule([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a descrption of your project:',
+    },
+    {
+        type: 'input',
+        name: 'tableOfContents',
+        message: 'Provide a table of contents:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?',
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What is the project license?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'How can others contribute to this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide information about tests:',
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'How can users reach out with questions?',
+    },
+])
+.then((answers) => {
+    // Generate the README file
+});
